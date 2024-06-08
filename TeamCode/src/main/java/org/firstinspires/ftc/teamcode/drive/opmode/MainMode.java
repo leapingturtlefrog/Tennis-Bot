fb879ce004aadacc595f2085d608208631184273
Alex Aridgides <Alexanderaridgides@gmail.com>
2024-06-08 15:43:55 -0400

    add tfood and aprilfools tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MainMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MainMode.java
@@ -13,6 +13,17 @@ import org.firstinspires.ftc.teamcode.drive.Robot;
 
 import org.firstinspires.ftc.teamcode.drive.PoseStorage;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
+
 /**
  * This opmode demonstrates how one can augment driver control by following Road Runner arbitrary
  * Road Runner trajectories at any time during teleop. This really isn't recommended at all. This is
@@ -39,7 +50,7 @@ import org.firstinspires.ftc.teamcode.drive.PoseStorage;
  * <p>
  * This sample utilizes the SampleMecanumDriveCancelable.java class.
  */
-@TeleOp(group = "pushbot")
+@TeleOp(group = "APushBot")
 //@Disabled
 public class MainMode extends LinearOpMode {
     // Define 2 states, drive control or automatic control
@@ -61,25 +72,29 @@ public class MainMode extends LinearOpMode {
     // The angle we want to align to when we press Y
     double targetAngle = Math.toRadians(45);
 
-
-    //
-
-
     Trajectory idle, move, collect, goHome, tragectory1, trajectory2; //etc
 
     Pose2d startPose;
 
     ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
 
-    enum State {
-
+    enum DriveState {
         IDLE,
         MOVE,
         COLLECT,
-        GO_HOME
+        HOME
     }
 
-    State currentState;
+    DriveState currentDriveState = DriveState.IDLE;
+
+    enum ImageState {
+        IDLE,
+        MOVE,
+        COLLECT,
+        HOME
+    }
+
+    ImageState currentImageState = ImageState.IDLE;
 
     enum StartPoseEnum {
         FRONT_LEFT,//left entrance towards the parking lot
@@ -88,11 +103,36 @@ public class MainMode extends LinearOpMode {
 
     StartPoseEnum startPoseEnum;
 
+    private static final String TFOD_MODEL_ASSET = "RedBlueBox.tflite";
+    // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
+    // this is used when uploading models directly to the RC using the model upload interface.
+    private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/RedBlueBox.tflite";
+    // Define the labels recognized in the model for TFOD (must be in training order!)
+    private static final String[] LABELS = {
+            "a",
+    };
+
+    /**
+     * The variable to store our instance of the TensorFlow Object Detection processor.
+     */
+    private TfodProcessor tfod;
+    private static final boolean USE_WEBCAM = true;
+    private AprilTagProcessor aprilTag;
+    private VisionPortal visionPortal;
+
+    private boolean tfodDetected = false;
+    private boolean aprilTagDetected = false;
+
+    private List<Recognition> tfodPositions;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
         // Initialize custom cancelable Robot class
         Robot drive = new Robot(hardwareMap);
+
+        initAprilTag();
+        initTfod();
 
         // We want to turn off velocity control for teleop
         // Velocity control per wheel is not necessary outside of motion profiled auto
@@ -102,22 +142,18 @@ public class MainMode extends LinearOpMode {
         // See AutoTransferPose.java for further details
         drive.setPoseEstimate(PoseStorage.currentPose);
 
-
-        //
-
-
         startPoseEnum = StartPoseEnum.FRONT_LEFT;
 
         switch (startPoseEnum) {
             case FRONT_LEFT:
 
-                startPose = new Pose2d(0, 100); //TODO: CHANGE VALUES
+                startPose = new Pose2d(0, 0,0); //TODO: CHANGE VALUES
 
                 break;
 
             default:
 
-                startPose = new Pose2d(0, 0);
+                startPose = new Pose2d(0, 0,0);
         }
 
         drive.setPoseEstimate(startPose);
@@ -129,8 +165,6 @@ public class MainMode extends LinearOpMode {
          */
 
         PoseStorage.currentPose = drive.getPoseEstimate();
-
-        currentState = State.IDLE;
 
         waitForStart();
 
@@ -144,7 +178,9 @@ public class MainMode extends LinearOpMode {
             Pose2d poseEstimate = drive.getPoseEstimate();
 
             // Print pose to telemetry
-            telemetry.addData("mode", currentMode);
+            telemetry.addData("mode: ", currentMode);
+            telemetry.addData("driveState: ", currentDriveState);
+            telemetry.addData("imageState: ", currentImageState);
             telemetry.addData("x", poseEstimate.getX());
             telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", poseEstimate.getHeading());
@@ -160,7 +196,30 @@ public class MainMode extends LinearOpMode {
                         currentMode = Mode.DRIVER_CONTROL;
                     }
 
-                    switch (currentState) {
+                    //image recognition
+                    switch (currentImageState) {
+                        case MOVE:
+                            tfodLocationDetection();
+                            telemetryTfod();
+
+                            break;
+
+                        case COLLECT:
+
+                            break;
+
+                        case HOME:
+                            aprilTagLocationDetection();
+                            telemetryAprilTag();
+
+                            break;
+
+                        default:
+
+                            break;
+                    }
+
+                    switch (currentDriveState) {
                         case MOVE:
                             //move to a tennis ball
                             drive.intakeOff();
@@ -182,7 +241,7 @@ public class MainMode extends LinearOpMode {
 
                             break;
 
-                        case GO_HOME:
+                        case HOME:
                             //head to home spot to drop of the tennis balls
                             drive.intakeOff();
 
@@ -249,5 +308,152 @@ public class MainMode extends LinearOpMode {
                     break;
             }
         }
+    }
+
+    private void initTfod() {
+
+        // Create the TensorFlow processor by using a builder.
+        tfod = new TfodProcessor.Builder()
+
+                // With the following lines commented out, the default TfodProcessor Builder
+                // will load the default model for the season. To define a custom model to load,
+                // choose one of the following:
+                //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
+                //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+                .setModelAssetName(TFOD_MODEL_ASSET)
+                //.setModelFileName(TFOD_MODEL_FILE)
+
+                // The following default settings are available to un-comment and edit as needed to
+                // set parameters for custom models.
+                .setModelLabels(LABELS)
+                .setIsModelTensorFlow2(true)
+                .setIsModelQuantized(true)
+                .setModelInputSize(300)
+                .setModelAspectRatio(16.0 / 9.0)
+
+                .build();
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(tfod);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Set confidence threshold for TFOD recognitions, at any time.
+        tfod.setMinResultConfidence(0.70f);
+
+        // Disable or re-enable the TFOD processor at any time.
+        //visionPortal.setProcessorEnabled(tfod, true);
+
+    }   // end method initTfod()
+    private void initAprilTag() {
+
+        // Create the AprilTag processor the easy way.
+        aprilTag = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTag);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTag);
+        }
+
+    }
+    private void telemetryTfod() {
+
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+        }   // end for() loop
+
+    }   // end method telemetryTfod()
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }
+
+    private void tfodLocationDetection()
+    {
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+
+        // Step through the list of detections and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+
+
+
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+        }
+
+
+    }
+    private void aprilTagLocationDetection()
+    {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+
+        }
+
+
     }
 }
